ad0bccdecf5e4c7363399cbd3ecebca9b0d196f2
John Yuan <John.YCQ@outlook.com>
2020-09-05 20:38:47 +0800

    store

--- bin/command.js
+++ bin/command.js
@@ -1,35 +1,26 @@
 #!/usr/bin/env node
 
-const fse = require('fs-extra');
-const path = require('path');
-const getAbsolutePath = require('../lib/shared/getAbsolutePath');
-const startStaticServer = require('../lib/server/startStaticServer');
-const buildStatic = require('../lib/builder/buildStatic');
-const { argv } = require('yargs')
-  .help(false)
-  .default('source', './')
-  .default('host', '0.0.0.0')
-  .default('port', 8080);
+const pick = require('lodash/fp/pick');
+const buildStaticPage = require('..');
+const { argv } = require('yargs').help(false);
 
-const helpFilePath = path.resolve(__dirname, 'help.txt');
-const helpContent = fse.readFileSync(helpFilePath).toString().trim();
-const sourceDir = getAbsolutePath(argv.source);
+const options = pick([
+  'init',
+  'build',
+  'serve',
+  'host',
+  'port',
+  'root',
+  'src',
+  'dist',
+  'config',
+  'mode',
+  'help',
+  'version',
+])(argv);
 
-if (argv.help) {
-  console.log(helpContent);
-} else if (argv.serve) {
-  startStaticServer(sourceDir, argv.port, argv.host, argv.mode)
-    .catch(err => {
-      console.error(err);
-      process.exit(1);
-    });
-} else if (argv.target) {
-  const targetDir = getAbsolutePath(argv.target);
-  buildStatic(sourceDir, targetDir, argv.mode)
-    .catch(err => {
-      console.error(err);
-      process.exit(1);
-    });
-} else {
-  console.log(helpContent);
-}
+buildStaticPage(options)
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
--- index.js
+++ index.js
@@ -1,7 +1,59 @@
-const buildStatic = require("./lib/builder/buildStatic");
-const startStaticServer = require("./lib/server/startStaticServer");
+const fse = require('fs-extra');
+const path = require('path');
+const propOr = require('lodash/fp/propOr');
+const package = require('./package.json');
+const buildStatic = require('./lib/buildStatic');
+const serveStatic = require('./lib/serveStatic');
+const parseOptions = require('./lib/shared/parseOptions');
+const initProject = require('./lib/initProject');
 
-module.exports = {
-  buildStatic: buildStatic,
-  startStaticServer: startStaticServer
-};
+/**
+ * Build static page options
+ *
+ * @typedef {Object} BuildStaticPageOptions
+ * @property {boolean} init Initialize project
+ * @property {boolean} build Build static page
+ * @property {boolean} serve Start development server
+ * @property {string} [host=0.0.0.0] Server host to use
+ * @property {number} [port=8080] Server port to use
+ * @property {string} [root=./] Project root directory
+ * @property {string} [src=./www] Project src directory
+ * @property {string} [dist=./dist] Project dist directory
+ * @property {string} [config=./build.static.page.config.js] Path to configuration
+ * @property {string} [mode=development|production] Set mode
+ * @property {boolean} [version] Print version
+ * @property {boolean} [help] Print help
+ */
+
+/**
+ * Build static pages
+ *
+ * @param {BuildStaticPageOptions} options
+ */
+function buildStaticPage(options) {
+  return new Promise((resolve) => {
+    const parsedOptions = parseOptions(options);
+
+    if (parseOptions) {
+      if (parsedOptions.init) {
+        return initProject(parsedOptions);
+      } else if (parsedOptions.build) {
+        parsedOptions.mode = propOr('production', 'mode')(parsedOptions);
+        return buildStatic(parsedOptions);
+      } else if (parsedOptions.serve) {
+        parsedOptions.mode = propOr('development', 'mode')(parsedOptions);
+        return serveStatic(parsedOptions);
+      } else if (parsedOptions.version) {
+        console.log(package.version);
+        resolve(package.version);
+      } else {
+        const helpFilePath = path.resolve(__dirname, './bin/help.txt');
+        const helpText = fse.readFileSync(helpFilePath).toString().trim();
+        console.log(helpText);
+        resolve(helpText);
+      }
+    }
+  });
+}
+
+module.exports = buildStaticPage;
